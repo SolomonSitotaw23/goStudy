74f0d314f1fc4a7dbc24cac7ffe4a07392840762
Solomon Sitotaw <solomonsitotaw9@gmail.com>
2025-06-01 23:04:41 +0300

    refactored the sum func to add a slice of any size

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,11 +3,14 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 1, 1, 1, 1}
-	got := Sum(numbers)
-	expected := 5
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 1, 1, 1, 1, 1}
+		got := Sum(numbers)
+		expected := 6
 
-	if got != expected {
-		t.Errorf("expected %d but got %d", expected, got)
-	}
+		if got != expected {
+			t.Errorf("expected %d but got %d given %v", expected, got, numbers)
+		}
+	})
+
 }
