9c705db247b37dd00de8a7fa09874eae29f52fd1
Solomon Sitotaw <solomonsitotaw9@gmail.com>
2025-06-02 12:05:37 +0300

    refactor to use append and added go doc on SumAll func

--- arrays/sumAll.go
+++ arrays/sumAll.go
@@ -1,11 +1,13 @@
 package arrays
 
+// accepts any number of slices and return a
+// slice with the sum of each slice
 func SumAll(numbersToSum ...[]int) (sums []int) {
 
 	sums = make([]int, len(numbersToSum))
 
-	for i, number := range numbersToSum {
-		sums[i] = Sum(number)
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
 	}
 
 	return
