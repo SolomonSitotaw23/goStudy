28ba85d8f5cee30f33401930bd871ff1ed8a10f2
Solomon Sitotaw <solomonsitotaw9@gmail.com>
2025-06-06 00:12:57 +0300

    Add error handler when searching unknown words and also test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,14 +3,41 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{
+
+	dictionary := Dictionary{
 		"test": "this is just a test",
 	}
+	t.Run("known word", func(t *testing.T) {
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		want := ErrNotFound
+
+		if got == nil {
+			t.Fatal("Expected to get an error.")
+		}
+
+		assertError(t, got, want)
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q given, %q ", got, want, "test")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
