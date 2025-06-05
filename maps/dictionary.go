e68e0a3a3780a15ea44353c22131ac6136c83026
Solomon Sitotaw <solomonsitotaw9@gmail.com>
2025-06-06 00:56:52 +0300

    Add a new method to add a word and definition on a dictionary and a test to search the newly added word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -15,3 +15,13 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 	return definition, nil
 }
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
+
+// Note on maps
+//  -> a nill map (var m map[string]string) will act like an empty map while reading
+// but it is a run time error if you tried to write
+//  eg m["hello"]="test" -> is a runtime error
+// maps should be created like var m = map[string]string{} or var m = make(map[string]string)
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -25,6 +25,29 @@ func TestSearch(t *testing.T) {
 
 		assertError(t, got, want)
 	})
+
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+
+	t.Helper()
+
+	got, err := dictionary.Search("word")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, definition)
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
